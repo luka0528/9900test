a5918467cfb983d8b6f4a7b20a07be8a94010b28
Parv Rehan <parvyyy@Mac.hub>
2025-03-10 20:29:56 +1100

    F13ABANANA-195 Created Marketplace skeleton layout

--- src/app/marketplace/page.tsx
+++ src/app/marketplace/page.tsx
@@ -1,5 +1,29 @@
-// TODO: Parv + Utsav put your marketplace page here.
-
 export default function Marketplace() {
-  return <div>Marketplace</div>;
+  const services = Array.from({ length: 50 }, (_, i) => ({
+    id: i,
+    name: `Service ${i}`,
+  }));
+
+  return (
+      <div className="flex w-full h-full xl:max-w-[96rem]">
+        <div className="w-48 h-full border-r">
+          SideBar
+        </div>
+        <div className="flex flex-col grow h-full">
+          <div className="flex min-h-20 justify-between items-center">
+            <div className="w-40 ml-8 h-8">Sort by Popularity</div>
+            <div className="w-60 mr-12 h-8 text-right"> Search Bar </div>
+          </div>
+          <div className="grid grid-cols-2 grow px-8 pb-8 gap-16 overflow-y-auto">
+            {
+              services.map((s) => (
+                <div key={s.id} className="p-4 h-60 border">
+                  <h2>{s.name}</h2>
+                </div>
+              ))
+            }
+          </div>
+        </div>
+      </div>
+  );
 }
