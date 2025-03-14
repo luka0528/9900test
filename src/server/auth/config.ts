3bb34243e4309067af32c48a474aa103a7d75cbb
Eoin Redoblado <innie.red@gmail.com>
2025-03-14 13:58:20 +1100

    Google auth

--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -1,6 +1,7 @@
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { type DefaultSession, type NextAuthConfig } from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
+import GoogleProvider from "next-auth/providers/google";
 import CredentialsProvider from "next-auth/providers/credentials";
 
 import { db } from "~/server/db";
@@ -35,6 +36,7 @@ declare module "next-auth" {
 export const authConfig = {
   providers: [
     DiscordProvider,
+    GoogleProvider,
     CredentialsProvider({
       // The name to display on the sign-in form (e.g., "Sign in with...")
       name: "Credentials",
