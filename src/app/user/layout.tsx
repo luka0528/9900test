74662d2663fe0f1fd290c43aa48ae972992e79d0
Edward R <66365396+diddlemeister@users.noreply.github.com>
2025-03-15 01:30:37 +1100

    F13ABANANA-139 moved sidebar to be defined within user/[userId]

--- src/app/user/layout.tsx
+++ src/app/user/layout.tsx
@@ -1,8 +1,6 @@
 import { HydrateClient } from "~/trpc/server";
 import NavBar from "~/components/navbar/NavBar";
-import SideBar, { SidebarItem } from "~/components/sidebar/SideBar";
 import { SessionProvider } from "next-auth/react";
-import { Settings, Package } from "lucide-react";
 import React from "react";
 
 export default function UserLayout({
@@ -10,11 +8,6 @@ export default function UserLayout({
 }: {
   children: React.ReactNode;
 }) {
-  // Define sidebar menu items
-  const sidebarItems: SidebarItem[] = [
-    { name: "Settings", href: "/settings", icon: <Settings /> },
-    { name: "Services", href: "/services", icon: <Package /> }
-  ];
 
   return (
     <SessionProvider>
@@ -23,7 +16,6 @@ export default function UserLayout({
           <NavBar />
         </HydrateClient>
         <div className="flex flex-1 min-h-0">
-          <SideBar items={sidebarItems} />
           <div className="flex-1">{children}</div>
         </div>
       </div>
