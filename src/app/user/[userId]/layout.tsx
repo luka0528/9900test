3c339c63cdc70c81b09f5b6c14f3d2d4719949d0
Edward Roy <edward200roy@gmail.com>
2025-03-15 18:49:42 +1100

    F13ABANANA-139 Added breadcrumb functionality within sidebar for UX

--- src/app/user/[userId]/layout.tsx
+++ src/app/user/[userId]/layout.tsx
@@ -4,6 +4,7 @@ import { useParams } from "next/navigation";
 import { SidebarProvider, SidebarTrigger } from "~/components/ui/sidebar"
 import { SideBar, SidebarItem } from "~/components/sidebar/SideBar"
 import { Package, Settings } from "lucide-react"
+import { breadCrumbItem } from "~/components/breadcrumb/BreadCrumb";
 
 export default function ProfileLayout({children,}: {children: React.ReactNode;}) {
     // Get userId from URL parameters
@@ -11,7 +12,7 @@ export default function ProfileLayout({children,}: {children: React.ReactNode;})
     const sessionId = useSession().data?.user.id;
 
     // Define sidebar menu items based on session user ID
-    const items: SidebarItem[] = sessionId === userId ? [
+    const sideBarItems: SidebarItem[] = sessionId === userId ? [
         { title: "Profile Settings", url: `/profile`, icon: Settings },
         { title: "My Services", url: `/services`, icon: Package }
     ] : [
@@ -19,9 +20,16 @@ export default function ProfileLayout({children,}: {children: React.ReactNode;})
         { title: "User Services", url: `/services`, icon: Package }
     ];
 
+    const breadCrumbItems: breadCrumbItem[] = [
+        { href: `/`, label: "Home" },
+        { href: `/user/${userId}`, label: "User" },
+        { href: `/user/${userId}/services`, label: sessionId === userId ? "My Services" : "Services" },
+        { href: `/user/${userId}/profile`, label: sessionId === userId ? "My Profile" : "Profile"  },
+    ];
+
     return (
         <SidebarProvider>
-          <SideBar items={items} label="Profile" baseUrl={`/user/${userId}`}/>
+          <SideBar sideBarItems={sideBarItems} label="Profile" baseUrl={`/user/${userId}`} breadCrumbItems={breadCrumbItems}/>
           <main>
             {children}
           </main>
--- src/components/sidebar/SideBar.tsx
+++ src/components/sidebar/SideBar.tsx
@@ -10,6 +10,7 @@ import {
     SidebarMenuButton,
     SidebarMenuItem,
   } from "~/components/ui/sidebar"
+import { BreadCrumb, BreadCrumbProps } from "../breadcrumb/BreadCrumb";
    
 export interface SidebarItem {
     title: string;
@@ -18,20 +19,53 @@ export interface SidebarItem {
 }
 
 interface SidebarProps {
-    items: SidebarItem[];
+    sideBarItems: SidebarItem[];
 }
 
-export function SideBar({ items = [], label = "Sidebar", baseUrl = "" }: SidebarProps & { label?: string } & { baseUrl?: string }) {
+/**
+ * SideBar component that renders a sidebar with breadcrumb navigation and menu items.
+ *
+ * @param {SidebarProps & BreadCrumbProps & { label?: string, baseUrl?: string }} props - The props for the SideBar component.
+ * @param {Array} props.sideBarItems - An array of sidebar items to be displayed.
+ * @param {Array} props.breadCrumbItems - An array of breadcrumb items to be displayed.
+ * @param {string} [props.label="Sidebar"] - The label for the sidebar.
+ * @param {string} [props.baseUrl=""] - The base URL to be prefixed to each sidebar item URL.
+ *
+ * @returns {JSX.Element} The rendered SideBar component.
+ *
+ * @example
+ * ```tsx
+ * const sideBarItems = [
+ *   { title: 'Home', url: '/home', icon: HomeIcon },
+ *   { title: 'Profile', url: '/profile', icon: ProfileIcon },
+ * ];
+ * 
+ * const breadCrumbItems: breadCrumbItem[] = [
+ *        { href: `/`, label: "Home" },
+ *        { href: `/user/${userId}`, label: "User" },
+ *        { href: `/user/${userId}/services`, label: sessionId === userId ? "My Services" : "Services" },
+ *        { href: `/user/${userId}/profile`, label: sessionId === userId ? "My Profile" : "Profile"  },
+ * ];
+ *
+ * <SideBar
+ *   sideBarItems={sideBarItems}
+ *   breadCrumbItems={breadCrumbItems}
+ *   label="Main Sidebar"
+ *   baseUrl="/app"
+ * />
+ * ```
+ */
+export function SideBar({ sideBarItems = [], breadCrumbItems=[], label = "Sidebar", baseUrl = "",  }: SidebarProps & BreadCrumbProps & { label?: string } & { baseUrl?: string }) {
     // Get the current pathname
     const pathname = usePathname();
     return (
         <Sidebar collapsible="none" className="h-full min-h-screen">
             <SidebarContent className="flex-1">
                 <SidebarGroup>
-                    <SidebarGroupLabel>{label}</SidebarGroupLabel>
+                    <div className="mb-4"><BreadCrumb breadCrumbItems={breadCrumbItems} /></div>
                     <SidebarGroupContent>
                         <SidebarMenu>
-                            {items.map((item) => {
+                            {sideBarItems.map((item) => {
                                 const isActive = pathname === `${baseUrl}${item.url}`;
                                 // console.error(`pathname: ${pathname} === ${baseUrl}${item.url} = ${isActive}`);
                                 return (
