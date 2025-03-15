29013c89a400cf456369184b2c82a2d29a5555d8
Aditya <121993233+xadta@users.noreply.github.com>
2025-03-16 00:25:02 +1100

    Preliminary implementation for documentation by version, progress on getInfoById

--- src/server/api/routers/service.ts
+++ src/server/api/routers/service.ts
@@ -1,3 +1,4 @@
+import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 
 import {
@@ -29,4 +30,86 @@ export const serviceRouter = createTRPCRouter({
     const services = await ctx.db.service.findMany();
     return services;
   }),
+
+  getInfoById: publicProcedure
+    .input(z.string().min(1))
+    .query(async ({ ctx, input }) => {
+      const service = await ctx.db.service.findUnique({
+        where: {
+          id: input,
+        },
+        include: {
+          tags: true,
+          versions: true,
+          owners: {
+            include: { user: true },
+          },
+          ratings: {
+            include: {
+              consumer: { include: { user: true } },
+              comments: true,
+            },
+          },
+        },
+      });
+
+      if (!service) {
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: "Service not found",
+        });
+      }
+
+      const serviceTags = service.tags.map((tag) => tag.name);
+
+      const serviceVersions = service.versions.map(
+        (version) => version.version,
+      );
+      const ownerNames = service.owners.map((owner) => owner.user.name);
+      // Get ratings
+      // [
+      //	{
+      //			consumerName, starValue, content, createdAt, comments: [{ownerName, content, createdAt}]
+      //	}
+      // ]
+    }),
+
+  getDocumentationByVersion: publicProcedure
+    .input(
+      z.object({
+        serviceId: z.string().min(1),
+        serviceVersion: z.string().min(1),
+      }),
+    )
+    .query(async ({ ctx, input }) => {
+      // Check service with this version exists
+      const service = await ctx.db.service.findUnique({
+        where: {
+          id: input.serviceId,
+        },
+        select: {
+          versions: {
+            where: {
+              version: input.serviceVersion,
+            },
+          },
+        },
+      });
+
+      if (!service) {
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: "Service not found",
+        });
+      }
+
+      // Should be an array of length one
+      if (service.versions.length !== 1) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: "The version does not exist",
+        });
+      }
+      return service.versions[0]!.description;
+    }),
 });
