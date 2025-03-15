ae1d416b260b0504b2528d5d55cc57e58d611314
Teo Mateo <z5416145@email>
2025-03-15 16:39:21 +1100

    basic skeleton finished (service page)

--- src/app/service/[serviceId]/page.tsx
+++ src/app/service/[serviceId]/page.tsx
@@ -1,36 +1,141 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useParams } from "next/navigation";
 import { Button } from "~/components/ui/button";
+import { Badge } from "~/components/ui/badge";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "~/components/ui/dropdown-menu";
-import { Pencil, ChevronDown, Heart, HeartOff } from "lucide-react";
+import { Pencil, ChevronDown, Heart, HeartOff, Tags } from "lucide-react";
 
 export default function ServicePage() {
   const [isSaved, setIsSaved] = useState(false);
+  const { serviceId } = useParams();
 
   // Admin only
   const isAdmin = true;
 
-  // Example versions data
-  const versions = [
-    { id: "v1.0", name: "Version 1.0" },
-    { id: "v1.1", name: "Version 1.1" },
-    { id: "v2.0", name: "Version 2.0" },
-    { id: "v2.1", name: "Version 2.1 (Current)" },
+  // Dummy Data
+  const services = [
+    {
+      id: 1,
+      name: "Service 1",
+      versions: [
+        {
+          vid: "v0.0",
+          name: "Version 0.0",
+          description:
+            "starting with 0.0, this is the first version of the service",
+          details: [
+            {
+              title: "What is Lorem Ipsum?",
+              content: "nothing here yet",
+            },
+          ],
+        },
+        {
+          vid: "v1.0",
+          name: "Version 1.0",
+          description:
+            "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...",
+          details: [
+            {
+              title: "What is Lorem Ipsum?",
+              content:
+                "Lorem ipsum odor amet, consectetuer adipiscing elit. Dui posuere adipiscing natoque parturient dapibus nunc orci mollis laoreet. Dolor risus sociosqu blandit tortor iaculis condimentum imperdiet. Dolor non fermentum curabitur maecenas consequat. Maecenas sollicitudin neque id pharetra fames risus turpis. Felis luctus hac habitant; conubia viverra et. Facilisis tincidunt sodales donec ultricies vehicula dapibus tristique. Finibus congue tristique sodales donec conubia. Mattis vehicula feugiat morbi sodales sit urna.",
+            },
+            {
+              title: "Where does it come from?",
+              content:
+                "Ad iaculis lectus senectus sapien nisl sem. Purus posuere montes rutrum dis; aptent consectetur molestie! Condimentum himenaeos nascetur bibendum nisl, odio ornare donec ullamcorper condimentum. Dictumst gravida aptent faucibus placerat vulputate taciti montes montes. Convallis ac sit nulla accumsan posuere vulputate nunc maecenas mollis. Rutrum curabitur euismod sagittis fusce at eros.",
+            },
+            {
+              title: "Code Table",
+              content: "Code Table Content",
+              table: [
+                { code: "Code 1", description: "Description 1" },
+                { code: "Code 2", description: "Description 2" },
+                { code: "Code 3", description: "Description 3" },
+              ],
+            },
+          ],
+          tags: ["meow", "woof", "fish"],
+        },
+        {
+          vid: "v1.1",
+          name: "Version 1.1 (latest)",
+          description:
+            "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...",
+          details: [
+            {
+              title: "What is Lorem Ipsum?",
+              content:
+                "Lorem ipsum odor amet, consectetuer adipiscing elit. Dui posuere adipiscing natoque parturient dapibus nunc orci mollis laoreet. Dolor risus sociosqu blandit tortor iaculis condimentum imperdiet. Dolor non fermentum curabitur maecenas consequat. Maecenas sollicitudin neque id pharetra fames risus turpis. Felis luctus hac habitant; conubia viverra et. Facilisis tincidunt sodales donec ultricies vehicula dapibus tristique. Finibus congue tristique sodales donec conubia. Mattis vehicula feugiat morbi sodales sit urna.",
+            },
+            {
+              title: "Where does it come from?",
+              content:
+                "Ad iaculis lectus senectus sapien nisl sem. Purus posuere montes rutrum dis; aptent consectetur molestie! Condimentum himenaeos nascetur bibendum nisl, odio ornare donec ullamcorper condimentum. Dictumst gravida aptent faucibus placerat vulputate taciti montes montes. Convallis ac sit nulla accumsan posuere vulputate nunc maecenas mollis. Rutrum curabitur euismod sagittis fusce at eros.",
+            },
+            {
+              title: "Code Table",
+              content: "Code Table Content",
+              table: [
+                { code: "Code 1", description: "Description 1" },
+                { code: "Code 2", description: "Description 2" },
+                { code: "Code 3", description: "Description 3" },
+              ],
+            },
+            {
+              title: "Where does it come from?",
+              content:
+                "Ad iaculis lectus senectus sapien nisl sem. Purus posuere montes rutrum dis; aptent consectetur molestie! Condimentum himenaeos nascetur bibendum nisl, odio ornare donec ullamcorper condimentum. Dictumst gravida aptent faucibus placerat vulputate taciti montes montes. Convallis ac sit nulla accumsan posuere vulputate nunc maecenas mollis. Rutrum curabitur euismod sagittis fusce at eros.",
+            },
+            {
+              title: "Code Table 2",
+              content: "Code Table Content",
+              table: [
+                { code: "Code 4", description: "Description 4" },
+                { code: "Code 5", description: "Description 5" },
+                { code: "Code 6", description: "Description 6" },
+              ],
+            },
+          ],
+          tags: ["meow", "woof", "fish", "glizzy"],
+        },
+      ],
+    },
   ];
+
+  // State to track seletected version
+  const [selectedVersion, setSelectedVersion] = useState(
+    services[0]?.versions[0],
+  );
+
+  const service = services.find((s) => s.id === parseInt(serviceId as string));
+  const versions = service?.versions || [];
+
+  useEffect(() => {
+    if (versions.length > 0) {
+      setSelectedVersion(versions[versions.length - 1]);
+    }
+  }, []);
+
+  const handleVersionSelect = (version: any) => {
+    setSelectedVersion(version);
+  };
 
   return (
     <div className="flex h-full w-full xl:max-w-[96rem]">
-      <div className="h-full w-48 border-r">SideBar</div>
+      <div className="h-full border-r lg:min-w-60">SideBar</div>
       <div className="flex h-full grow flex-col">
         <div className="border-b p-4">
           <div className="mb-4 flex items-center justify-between">
-            <h1 className="text-2xl font-bold">Lorem Ipsum </h1>
+            <h1 className="text-2xl font-bold">{service?.name}</h1>
             <div className="flex items-center gap-2">
               <Button>Support</Button>
               <Button size="icon" onClick={() => setIsSaved(!isSaved)}>
@@ -42,16 +147,15 @@ export default function ServicePage() {
               <DropdownMenu>
                 <DropdownMenuTrigger asChild>
                   <Button variant="outline" className="flex items-center gap-2">
-                    Version <ChevronDown className="text-muted-foreground" />
+                    {selectedVersion?.name}{" "}
+                    <ChevronDown className="text-muted-foreground" />
                   </Button>
                 </DropdownMenuTrigger>
                 <DropdownMenuContent align="end">
                   {versions.map((version) => (
                     <DropdownMenuItem
-                      key={version.id}
-                      onClick={() =>
-                        console.log(`Selected version: ${version.id}`)
-                      }
+                      key={version.vid}
+                      onClick={() => handleVersionSelect(version)}
                     >
                       {version.name}
                     </DropdownMenuItem>
@@ -60,69 +164,58 @@ export default function ServicePage() {
               </DropdownMenu>
             </div>
           </div>
-          <p className="mb-4">
-            "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet,
-            consectetur, adipisci velit..."
-          </p>
+          {selectedVersion?.tags && (
+            <div className="mb-8 flex items-center gap-2">
+              {selectedVersion.tags.map((tag: string) => (
+                <Badge key={tag} variant="outline">
+                  {tag}
+                </Badge>
+              ))}
+            </div>
+          )}
+          <p className="mb-4">{selectedVersion?.description}</p>
         </div>
 
         <div className="flex-grow p-4">
-          <h2 className="mb-4 text-xl font-semibold">What is Lorem Ipsum?</h2>
-          <p className="mb-4">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged. It was popularised in the 1960s
-            with the release of Letraset sheets containing Lorem Ipsum passages,
-            and more recently with desktop publishing software like Aldus
-            PageMaker including versions of Lorem Ipsum.
-          </p>
+          {selectedVersion?.details.map((detail: any, index: number) => (
+            <div key={index} className="mb-12">
+              <h2 className="mb-4 text-xl font-semibold">{detail.title}</h2>
 
-          <h2 className="mb-4 text-xl font-semibold">
-            Where does it come from?
-          </h2>
-          <p className="mb-4">
-            Contrary to popular belief, Lorem Ipsum is not simply random text.
-            It has roots in a piece of classical Latin literature from 45 BC,
-            making it over 2000 years old. Richard McClintock, a Latin professor
-            at Hampden-Sydney College in Virginia, looked up one of the more
-            obscure Latin words, consectetur, from a Lorem Ipsum passage, and
-            going through the cites of the word in classical literature,
-            discovered the undoubtable source. Lorem Ipsum comes from sections
-            1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes
-            of Good and Evil) by Cicero, written in 45 BC. This book is a
-            treatise on the theory of ethics, very popular during the
-            Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit
-            amet..", comes from a line in section 1.10.32.
-          </p>
-
-          <h2 className="mb-4 text-xl font-semibold">Code Table</h2>
-          <div className="overflow-x-auto">
-            <table className="min-w-full border">
-              <thead>
-                <tr>
-                  <th
-                    colSpan={2}
-                    className="border bg-gray-100 px-4 py-2 text-left"
-                  >
-                    Methods
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                {Array.from({ length: 10 }).map((_, index) => (
-                  <tr key={index}>
-                    <td className="w-1/4 border px-4 py-2">Code {index + 1}</td>
-                    <td className="w-3/4 border px-4 py-2">
-                      Description {index + 1}
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
+              {detail.table ? (
+                <div className="overflow-x-auto">
+                  <p className="mb-4">{detail.content}</p>
+                  <table className="min-w-full border">
+                    <thead>
+                      <tr>
+                        <th
+                          colSpan={2}
+                          className="border bg-gray-100 px-4 py-2 text-left"
+                        >
+                          Methods
+                        </th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {detail.table.map((row: any, idx: number) => (
+                        <tr key={idx}>
+                          <td className="w-1/4 border px-4 py-2">
+                            <code className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold">
+                              {row.code}
+                            </code>
+                          </td>
+                          <td className="w-3/4 border px-4 py-2">
+                            {row.description}
+                          </td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                </div>
+              ) : (
+                <p className="mb-4">{detail.content}</p>
+              )}
+            </div>
+          ))}
         </div>
       </div>
     </div>
