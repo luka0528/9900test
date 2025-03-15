67f66bf7753917e45fd57fdb310330437898c654
Parv Rehan <parvyyy@Mac.hub>
2025-03-15 20:28:37 +1100

    F13ABANANA-216 Completed design for Price Filtering

--- src/app/marketplace/MarketplaceSidebar.tsx
+++ src/app/marketplace/MarketplaceSidebar.tsx
@@ -6,6 +6,8 @@ import {
 } from "~/components/ui/accordion";
 
 import { MarketplaceTagsFilter } from "./MarketplaceTagsFilter";
+import { MarketplacePriceFilter } from "./MarketplacePriceFilter";
+import { MarketplaceDateFilter } from "./MarketplaceDateFilter";
 
 export const MarketplaceSideBar = () => {
     return (
@@ -15,6 +17,12 @@ export const MarketplaceSideBar = () => {
                     <AccordionTrigger>Tags</AccordionTrigger>
                     <AccordionContent >
                         <MarketplaceTagsFilter />
+                    </AccordionContent>
+                </AccordionItem>
+                <AccordionItem value="item-2">
+                    <AccordionTrigger>Price</AccordionTrigger>
+                    <AccordionContent >
+                        <MarketplacePriceFilter />
                     </AccordionContent>
                 </AccordionItem>
             </Accordion>
