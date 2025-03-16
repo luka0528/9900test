2739c264fa89ca2cb1308a11d09494342a644889
Parv Rehan <parvyyy@Mac.hub>
2025-03-16 13:26:42 +1100

    F13ABANANA-221 Updated tags to use search params

--- src/components/marketplace/MarketplaceTagsFilter.tsx
+++ src/components/marketplace/MarketplaceTagsFilter.tsx
@@ -5,43 +5,38 @@ import {
   ToggleGroup,
   ToggleGroupItem,
 } from "~/components/ui/toggle-group"
-import { MarketplaceContext } from "./MarketplaceContext";
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 
 const tagList = ["Transport", "Education", "Commerce", "Health", "Finance", "Entertainment", "Food", "Travel", "Sports", "Technology", "Fashion", "Art", "Music", "Science", "Environment", "Politics"];
 
-interface MarketplaceTagsFilterProps {
-    opened: boolean;
-}
+export const MarketplaceTagsFilter = () => {
+    const searchParams = useSearchParams();
+    const pathname = usePathname();
+    const { replace } = useRouter();
 
-export const MarketplaceTagsFilter = ({ opened }: MarketplaceTagsFilterProps) => {
-    const { query, setQuery, setIsToQuery } = React.useContext(MarketplaceContext);
-    const [tags, setTags] = React.useState<string[]>([]);
-    const [isOpened, setIsOpened] = React.useState(opened);
+    const [tags, setTags] = React.useState<string[]>(
+        searchParams.get('tags')?.split(',').filter(Boolean) || []
+    );
 
-    React.useEffect(() => {
-        // Prevents refetching when the component mounts
-        if (isOpened) {
-            setIsOpened(false);
-            return;
+    const handleTagsChanged = (newTags: string[]) => {
+        setTags(newTags);
+
+        const params = new URLSearchParams(searchParams.toString());
+        if (newTags.length > 0) {
+            params.set('tags', newTags.join(','));
+        } else {
+            params.delete('tags');
         }
-
-        setQuery({
-            ...query,
-            filters: {
-                ...query.filters,
-                tags,
-            }
-        });
-
-        setIsToQuery(true);
-    }, [tags]);
+        
+        replace(`${pathname}?${params.toString()}`);
+    }
 
     return (
         <div className="mt-2">
             <ToggleGroup 
                 type="multiple" 
                 value={tags}
-                onValueChange={setTags}
+                onValueChange={handleTagsChanged}
                 className="flex flex-wrap gap-2"
             >
                 {tagList.map((tag) => (
