043ee7aa16fd99a1cb55291a8c9c780a864dba44
Aditya <121993233+xadta@users.noreply.github.com>
2025-03-17 13:58:21 +1100

    Preliminary implementation for edit service name

--- src/server/api/routers/service.ts
+++ src/server/api/routers/service.ts
@@ -8,6 +8,7 @@ import {
 } from "~/server/api/trpc";
 
 import type { Query } from "~/components/marketplace/MarketplaceQuery";
+import { TRPCError } from "@trpc/server";
 
 export const serviceRouter = createTRPCRouter({
   // TODO: There'll be a lot more input here to create a service, this is just a placeholder
@@ -28,35 +29,86 @@ export const serviceRouter = createTRPCRouter({
       return service;
     }),
 
-    getInfiniteServices: publicProcedure
-      .input(
-        z.object({
-          query: z.custom<Query>(),
-          cursor: z.number().nullish(),
-        })
-      )
-      .query(async ({ input }) => {
-        const cursor = input.cursor ?? 0;
-        const limit = 12;
+  getInfiniteServices: publicProcedure
+    .input(
+      z.object({
+        query: z.custom<Query>(),
+        cursor: z.number().nullish(),
+      }),
+    )
+    .query(async ({ input }) => {
+      const cursor = input.cursor ?? 0;
+      const limit = 12;
 
-        // Generates fake services as mock data.
-        const services: Service[] = Array.from({ length: limit }, (_, i) => {
-          const id = cursor + i;
-          return {
-            id: id.toString(),
-            name: `Service ${id}`,
-            createdAt: new Date(),
-            updatedAt: new Date(),
-          };
-        });
+      // Generates fake services as mock data.
+      const services: Service[] = Array.from({ length: limit }, (_, i) => {
+        const id = cursor + i;
+        return {
+          id: id.toString(),
+          name: `Service ${id}`,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        };
+      });
 
-        const nextCursor = services.length ? cursor + limit : null;
+      const nextCursor = services.length ? cursor + limit : null;
 
-        return { services, nextCursor };
+      return { services, nextCursor };
     }),
 
   getAll: publicProcedure.query(async ({ ctx }) => {
     const services = await ctx.db.service.findMany();
     return services;
   }),
+
+  editName: protectedProcedure
+    .input(
+      z.object({ serviceId: z.string().min(1), newName: z.string().min(1) }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      // Check that user owns this service
+      const service = await ctx.db.service.findUnique({
+        where: {
+          id: input.serviceId,
+        },
+        include: {
+          owners: {
+            where: { userId: ctx.session.user.id },
+          },
+        },
+      });
+
+      // Check that the service exists and that the userId is in the owners list
+      if (!service) {
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: "Service not found",
+        });
+      }
+
+      // Edit the name
+      await ctx.db.service.update({
+        where: {
+          id: input.serviceId,
+        },
+        data: {
+          name: input.newName,
+        },
+      });
+
+      return { success: true };
+    }),
+
+  editDocumentation: protectedProcedure
+    .input(
+      z.object({
+        serviceId: z.string().min(1),
+        serviceVersion: z.string().min(1),
+        newDocumentation: z.string().min(1),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      // Check that user owns this service and that this version exists
+      // Edit the documentation
+    }),
 });
