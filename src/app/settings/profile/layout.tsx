62cba9be84bc287951293ad3a877b78a5f24085f
Teo Mateo <z5416145@email>
2025-04-08 16:41:46 +1000

    F13ABANANA-257 moved editprofile to settings path

---
+++ src/app/settings/profile/layout.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { ProfileSidebar } from "~/components/user/ProfileSidebar";
+import { useParams } from "next/navigation";
+export default function ProfileLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const { userId } = useParams();
+  return (
+    <div className="flex h-full w-full xl:max-w-[96rem]">
+      <ProfileSidebar userId={userId as string} />
+      <main className="h-full flex-1 overflow-hidden p-0">{children}</main>
+    </div>
+  );
+}
