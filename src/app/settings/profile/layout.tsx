579c768d83ba201ddce827ebc82725ebf08a88e7
Edward Roy <edward200roy@gmail.com>
2025-04-10 20:18:04 +1000

    ALDJGHSFD

--- src/app/settings/profile/layout.tsx
+++ src/app/settings/profile/layout.tsx
@@ -25,7 +25,7 @@ export default function ProfileLayout({
   return (
     <div className="flex h-full w-full xl:max-w-[96rem]">
       <DefaultSideBar items={items} />
-      <main className="h-full flex-1 p-0 overflow-y-auto">{children}</main>
+      <main className="h-full flex-1 overflow-y-auto p-0">{children}</main>
     </div>
   );
 }
