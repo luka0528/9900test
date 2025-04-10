e0ff4007b98527e44e67fa5ac359e2ab6fe37018
Parv Rehan <parvyyy@Mac.hub>
2025-04-10 22:16:42 +1000

    Made Skeleton FE for Review Cards

--- src/components/analytics/AnalyticsReviewSection.tsx
+++ src/components/analytics/AnalyticsReviewSection.tsx
@@ -1,11 +1,104 @@
 "use client";
 
 import { Card } from "~/components/ui/card";
+import { Skeleton } from "~/components/ui/skeleton";
+import { ExternalLink } from "lucide-react";
 
 export const AnalyticsReviewSection = () => {
   return (
-    <Card className="flex h-48 w-3/4 flex-col">
-      <div className="p-4">TODO</div>
-    </Card>
+  <div className="flex w-2/3 space-x-4">
+    <div className="space-y-4 pt-0 w-1/2">
+    <Card className="p-4">
+      <div className="flex items-center justify-between">
+        <div className="flex items-start space-x-4">
+        <Skeleton className="h-10 w-10 rounded-full" />
+        <div className="space-y-2">
+          <Skeleton className="h-4 w-[180px]" />
+          <Skeleton className="h-3 w-[130px]" />
+        </div>
+        </div>
+        <div className="cursor-pointer text-gray-400 hover:text-gray-600">
+          <ExternalLink size={18} />
+        </div>
+      </div>
+      </Card>
+
+      <Card className="p-4">
+      <div className="flex items-center justify-between">
+        <div className="flex items-start space-x-4">
+        <Skeleton className="h-10 w-10 rounded-full" />
+        <div className="space-y-2">
+          <Skeleton className="h-4 w-[180px]" />
+          <Skeleton className="h-3 w-[130px]" />
+        </div>
+        </div>
+        <div className="cursor-pointer text-gray-400 hover:text-gray-600">
+        <ExternalLink size={18} />
+        </div>
+      </div>
+      </Card>
+
+      <Card className="p-4">
+      <div className="flex items-center justify-between">
+        <div className="flex items-start space-x-4">
+        <Skeleton className="h-10 w-10 rounded-full" />
+        <div className="space-y-2">
+          <Skeleton className="h-4 w-[180px]" />
+          <Skeleton className="h-3 w-[130px]" />
+        </div>
+        </div>
+        <div className="cursor-pointer text-gray-400 hover:text-gray-600">
+          <ExternalLink size={18} />
+        </div>
+      </div>
+      </Card>
+    </div>
+    <div className="space-y-4 pt-0 w-1/2">
+      <Card className="p-4">
+      <div className="flex items-center justify-between">
+        <div className="flex items-start space-x-4">
+        <Skeleton className="h-10 w-10 rounded-full" />
+        <div className="space-y-2">
+          <Skeleton className="h-4 w-[180px]" />
+          <Skeleton className="h-3 w-[130px]" />
+        </div>
+        </div>
+        <div className="cursor-pointer text-gray-400 hover:text-gray-600">
+        <ExternalLink size={18} />
+        </div>
+      </div>
+      </Card>
+
+      <Card className="p-4">
+      <div className="flex items-center justify-between">
+        <div className="flex items-start space-x-4">
+        <Skeleton className="h-10 w-10 rounded-full" />
+        <div className="space-y-2">
+          <Skeleton className="h-4 w-[180px]" />
+          <Skeleton className="h-3 w-[130px]" />
+        </div>
+        </div>
+        <div className="cursor-pointer text-gray-400 hover:text-gray-600">
+        <ExternalLink size={18} />
+        </div>
+      </div>
+      </Card>
+
+      <Card className="p-4">
+      <div className="flex items-center justify-between">
+        <div className="flex items-start space-x-4">
+        <Skeleton className="h-10 w-10 rounded-full" />
+        <div className="space-y-2">
+          <Skeleton className="h-4 w-[180px]" />
+          <Skeleton className="h-3 w-[130px]" />
+        </div>
+        </div>
+        <div className="cursor-pointer text-gray-400 hover:text-gray-600">
+        <ExternalLink size={18} />
+        </div>
+      </div>
+      </Card>
+    </div>
+  </div>
   );
 };
