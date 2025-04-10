d67c7e952240055ec126019f08f4348519740be3
Parv Rehan <z5421600@ad.unsw.edu.au>
2025-04-10 19:12:50 +1000

    F13ABANANA-138 Continued FE of PieChart, positioning it & working on toggling between services

--- src/components/analytics/AnalyticsPieChart.tsx
+++ src/components/analytics/AnalyticsPieChart.tsx
@@ -16,13 +16,32 @@ import {
 import {
   ChartConfig,
   ChartContainer,
+  ChartLegend,
+  ChartLegendContent,
   ChartTooltip,
   ChartTooltipContent,
 } from "~/components/ui/charts"
 
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "~/components/ui/select"
+import { ToggleGroup, ToggleGroupItem } from "~/components/ui/toggle-group"
+
+// const chartData = [
+//   { browser: "chrome", visitors: 275, fill: "var(--color-chrome)" },
+//   { browser: "safari", visitors: 200, fill: "var(--color-safari)" },
+//   { browser: "firefox", visitors: 287, fill: "var(--color-firefox)" },
+//   { browser: "edge", visitors: 173, fill: "var(--color-edge)" },
+//   { browser: "other", visitors: 190, fill: "var(--color-other)" },
+// ]
+type ChartDataPoint = {
+  browser: string
+  visitors: number
+  fill: string
+}
+
 export const AnalyticsPieChart = () => {
   const [currService, setCurrService] = React.useState("Service A");
-  
+  const [chartData, setChartData] = React.useState<ChartDataPoint[]>();
+
   const {
     data: userServiceData,
     isLoading
@@ -48,7 +67,7 @@ export const AnalyticsPieChart = () => {
     for (let i = 0; i < serviceTiers.length; i++) {
       const tier = serviceTiers[i]!;
       config[tier.tierName] = {
-        label: `${tier.tierName} ($${tier.price.toFixed(2)})`,
+        label: tier.tierName,
         color: colors[i % colors.length]!,
       };
     }
@@ -56,49 +75,101 @@ export const AnalyticsPieChart = () => {
     return config as ChartConfig;
   }, [userServiceData]);
 
+  React.useEffect(() => {
+    if (isLoading) return;
+
+    setChartData(() => {
+      return (
+        userServiceData?.get(currService)?.map((tier) => ({
+          browser: tier.tierName,
+          visitors: tier.customerCount,
+          fill: chartConfig[tier.tierName]?.color ?? "#ccc"
+        }))!
+      )
+    })
+  }, [currService, userServiceData])
 
   const totalVisitors = React.useMemo(() => {
     if (!userServiceData) {
       return 0;
     }
-    console.log("currService", currService, userServiceData.get(currService) ?? 0);
 
     if (!userServiceData || !currService || !userServiceData.get(currService)) {
       return 0;
     }
+
     return userServiceData.get(currService)!.reduce(
-      (acc: number, curr) => acc + (curr.customerCount ?? 0), 
+      (acc: number, curr) => acc + (curr.customerCount ?? 0),
       0
     );
 
-    
+
   }, [userServiceData, currService])
 
-
   return (
-    <Card className="flex flex-col h-64 w-1/4">
+    <Card className="flex flex-col h-64 w-1/3">
       {
         isLoading ? (
           <div className="flex h-full w-full items-center justify-center">
             Loading...
           </div>
         ) : (
-          <CardContent className="flex-1 pb-0">
+          <>
+          <CardHeader className="p-0 h-8">
+          <div className="absolute right-4 top-4">
+            <ToggleGroup
+              type="single"
+              value={currService}
+              onValueChange={setCurrService}
+              variant="outline"
+              className="@[767px]/card:flex hidden"
+            >
+              <ToggleGroupItem value="365d" className="h-8 px-2.5">
+                Last Year
+              </ToggleGroupItem>
+              <ToggleGroupItem value="30d" className="h-8 px-2.5">
+                Last 30 days
+              </ToggleGroupItem>
+            </ToggleGroup>
+            <Select value={currService} onValueChange={setCurrService}>
+              <SelectTrigger className="flex w-40">
+                <SelectValue />
+              </SelectTrigger>
+              <SelectContent className="rounded-xl">
+                <SelectItem value="365d" className="rounded-lg">
+                  Last Year
+                </SelectItem>
+                <SelectItem value="30d" className="rounded-lg">
+                  Last 30 days
+                </SelectItem>
+              </SelectContent>
+            </Select>
+          </div>
+          </CardHeader>
+          <CardContent className="flex-1 grow p-0">
             <ChartContainer
               config={chartConfig}
-              className="mx-auto aspect-square max-h-[250px]"
+              className="aspect-square max-h-[220px] w-full"
             >
-              <PieChart>
+              <PieChart width={250} height={250}>
                 <ChartTooltip
                   cursor={false}
-                  content={<ChartTooltipContent hideLabel />}
+                  content={<ChartTooltipContent />}
+                />
+                <ChartLegend
+                  content={<ChartLegendContent />}
+                  layout="vertical"
+                  verticalAlign="bottom"
+                  align="right"
+                  className="flex flex-col items-start w-20 h-20 p-2 mr-8 mb-4"
                 />
                 <Pie
-                  data={userServiceData?.get(currService)!}
+                  data={chartData}
                   dataKey="visitors"
                   nameKey="browser"
                   innerRadius={60}
                   strokeWidth={5}
+                  cx="45%"
                 >
                   <Label
                     content={({ viewBox }) => {
@@ -133,6 +204,7 @@ export const AnalyticsPieChart = () => {
               </PieChart>
             </ChartContainer>
           </CardContent>
+          </>
         )
       }
     </Card>
