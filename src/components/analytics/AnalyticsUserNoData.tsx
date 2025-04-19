7d738afec02e1e232acb5179ade7dbfae4db0476
Parv Rehan <parvyyy@Mac.hub>
2025-04-19 18:08:06 +1000

    Made prettier

--- src/components/analytics/AnalyticsUserNoData.tsx
+++ src/components/analytics/AnalyticsUserNoData.tsx
@@ -1,34 +1,31 @@
 import {
-    Card,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-  } from "~/components/ui/card";
-  import { Button } from "~/components/ui/button";
-  import { CalendarOffIcon } from "lucide-react";
-  import Link from "next/link";
-  
-  export const AnalyticsUserNoData = () => {
-    return (
-      <div className="flex h-full w-full items-center justify-center p-8">
-        <Card className="m-4 border-none shadow-none">
-          <CardHeader className="text-center">
-            <div className="mx-auto mb-4 flex h-12 w-12 items-center justify-center rounded-full bg-muted">
-              <CalendarOffIcon className="h-6 w-6" />
-            </div>
-            <CardTitle className="text-xl">No Analytics Data</CardTitle>
-            <CardDescription>
-              You must have at least one service.
-            </CardDescription>
-          </CardHeader>
-          <CardFooter className="flex justify-center">
-            <Button asChild>
-              <Link href="/service/owned">Add Service</Link>
-            </Button>
-          </CardFooter>
-        </Card>
-      </div>
-    );
-  };
-  
+  Card,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card";
+import { Button } from "~/components/ui/button";
+import { CalendarOffIcon } from "lucide-react";
+import Link from "next/link";
+
+export const AnalyticsUserNoData = () => {
+  return (
+    <div className="flex h-full w-full items-center justify-center p-8">
+      <Card className="m-4 border-none shadow-none">
+        <CardHeader className="text-center">
+          <div className="mx-auto mb-4 flex h-12 w-12 items-center justify-center rounded-full bg-muted">
+            <CalendarOffIcon className="h-6 w-6" />
+          </div>
+          <CardTitle className="text-xl">No Analytics Data</CardTitle>
+          <CardDescription>You must have at least one service.</CardDescription>
+        </CardHeader>
+        <CardFooter className="flex justify-center">
+          <Button asChild>
+            <Link href="/service/owned">Add Service</Link>
+          </Button>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+};
--- src/components/analytics/dashboard/Dashboard.tsx
+++ src/components/analytics/dashboard/Dashboard.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { AnalyticsCards } from "~/components/analytics/dashboard/AnalyticsCards";
 import { AnalyticsChart } from "~/components/analytics/dashboard/AnalyticsChart";
@@ -9,24 +9,24 @@ import { api } from "~/trpc/react";
 import { AnalyticsUserNoData } from "../AnalyticsUserNoData";
 
 export const Dashboard = () => {
-    const { data: services, isLoading: servicesLoading } =
-        api.analytics.getServicesByUser.useQuery();
-    return (
-        <>
-        {servicesLoading ? (
-            <></>
-        ) : services && services.length === 0 ? (
-            <AnalyticsUserNoData />
-        ) : (
-            <div className="m-4 flex h-full grow flex-col">
-                <AnalyticsCards />
-                <AnalyticsChart />
-                <div className="flex space-x-4">
-                    <AnalyticsReviewSection />
-                    <AnalyticsPieChart />
-                </div>
-            </div>
-        )}
-        </>
-    )
-}
+  const { data: services, isLoading: servicesLoading } =
+    api.analytics.getServicesByUser.useQuery();
+  return (
+    <>
+      {servicesLoading ? (
+        <></>
+      ) : services && services.length === 0 ? (
+        <AnalyticsUserNoData />
+      ) : (
+        <div className="m-4 flex h-full grow flex-col">
+          <AnalyticsCards />
+          <AnalyticsChart />
+          <div className="flex space-x-4">
+            <AnalyticsReviewSection />
+            <AnalyticsPieChart />
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
